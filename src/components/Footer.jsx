11ca285c96ec399327edc73602c61382e1b3a2a0
juanpabloestrada <pablo.estrada.1983.18@gmail.com>
2023-12-30 02:04:25 -0600

    se realizo el carrito de compras

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h4><span> go2do.com.mx</span></h4>
+    </footer>
+  )
+}
